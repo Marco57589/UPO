7e1e6b2a18f1048a42aa23e57a95c6eb4fb96681
Marco57589 <50150327+Marco57589@users.noreply.github.com>
2022-10-13 22:44:19 +0200

    Update es1.c

--- Programmazione 1/1 Variabili/es1.c
+++ Programmazione 1/1 Variabili/es1.c
@@ -1,41 +1,12 @@
-<h1>1) Variabili
-
-
-
-## Build source
-
-```
-$ gcc file_name.c
-```
-
-## Run
-
-```
-$ ./file_name
-```
-
-## Table
-
-| Command | Description |
-| --- | --- |
-| `npm run push:gas` | just GAS files |
-| `npm run push:js` | just `sidebar.js` |
-| `npm run push:static` | just HTML & CSS |
-| `npm run push` | everything |
-
-# 1
-
-```c
 #include <stdio.h>
 
 void main(){
+	
     int x = 0;
 	int y = 0;
+	
+	x = 7;
+	y = 3;
 
-    x = 7;
-    y = 3;
-
-    printf("Prodotto: %d", x * y);
-
+	printf("Prodotto: %d", x * y);
 }
-```
