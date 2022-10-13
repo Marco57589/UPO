3bbd02a5b8c9dcc5efedd2fc4f7eca9746107883
Marco57589 <50150327+Marco57589@users.noreply.github.com>
2022-10-13 22:46:33 +0200

    Update es6.c

--- Programmazione 1/1 Variabili/es6.c
+++ Programmazione 1/1 Variabili/es6.c
@@ -8,9 +8,15 @@ float e ne stampi la somma,la media, il prodotto.
 #include <stdio.h>
 
 void main(){
+	
+    float num1 = 0;
+    float num2 = 0;
+    float num3 = 0;
+    float num4 = 0;
 
-    float num1, num2, num3, num4;
-    float somma, media, prodotto = 0;
+    float somma = 0;
+    float media = 0;
+    float prodotto = 0;
 
     printf("Inserisci num1:");
     scanf("%f", &num1);
@@ -20,11 +26,15 @@ void main(){
     scanf("%f", &num3);
     printf("Inserisci num4:");
     scanf("%f", &num4);
-
+	
+//Metodo 1:
     somma = num1+num2+num3+num4;
     media = (somma / 4);
     prodotto = num1 * num2 * num3 * num4;
-
+ 
     printf("Somma: %f \nMedia: %f \nProdotto %f\n", somma, media, prodotto);
+	
+//Metodo 2 (Senza creazione variabili dedicate):
+    printf("Somma: %f \nMedia: %f \nProdotto %f\n", (num1+num2+num3+num4), (somma / 4), (num1 * num2 * num3 * num4));
 
 }
