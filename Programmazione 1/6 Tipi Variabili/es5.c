518387be84f81926a867e2cdfda1ebd7c4702e0e
Marco57589 <50150327+Marco57589@users.noreply.github.com>
2023-01-11 10:16:47 +0100

    Update es5.c

--- Programmazione 1/6 Tipi Variabili/es5.c
+++ Programmazione 1/6 Tipi Variabili/es5.c
@@ -1,6 +1,5 @@
 /*
-  5. 5.
-    Scrivere un programma organizzato in quattro funzioni:
+  5. Scrivere un programma organizzato in quattro funzioni:
     - caricamento in un vettore con tappo (-1) di una sequenza di voti interi controllando che siano 
         compresi tra 18 e 31 (31 corrisponde a 30 e lode);
     - stampa del vettore;
@@ -9,15 +8,11 @@
         e incremento della media di 0.25 per ogni lode;
     - main: invoca le funzioni e stampa la media finale. 
 
-
-
 */
 #include <stdio.h>
 
 #define len 10
 #define tappo -1
-
-
 
 void stampa_vettore(int* vettore){
     int i = 0;
@@ -27,12 +22,35 @@ void stampa_vettore(int* vettore){
 }
 
 int calcola_lunghezza(int* vettore){
-    int k = 0;
+    int length = 0;
+
     for(int i = 0; vettore[i] != tappo; i++){
-        k++;
+        length++;
     }
 
-    return len;
+    return length;
+}
+
+void calcola_media(int lunghezza, int* vettore){
+    float media = 0;
+
+    for(int i = 0; vettore[i] != tappo; i++){
+        if(vettore[i] == 31){
+            media += (30*110) /30;
+        }else{
+            media += (vettore[i]*110) /30;
+        }
+    }
+
+    media =  media/lunghezza;
+
+    for(int i = 0; vettore[i] != tappo; i++){
+        if(vettore[i] == 31){
+            media += 0.25;
+        }
+    }
+
+    printf("\n La media e': %f", media);
 }
 
 int main(){
@@ -40,22 +58,24 @@ int main(){
     int num;
 
     for(int i = 0; i < len; i++){
-        printf("\nInserisci il voto > 18 < 32 (-1) per fermarsi): ", i);
+        printf("\nInserisci il voto [%d] che deve essere >= 18 <= 31 (-1) per fermarsi): ", i);
         scanf("%d", &num);
         if(num < 18 || num > 31){
             puts("Il valore deve essere compreso tra 18 e 31");
             i--;
-        }
-
-        if(num == tappo){
-            vettore[i] = tappo;
-            break;
         }else{
             vettore[i] = num;
         }
+
+        if(num == tappo){
+            vettore[i+1] = tappo;
+            break;
+        }
     }
 
-    stampa_vettore(&vettore);
+    stampa_vettore(vettore);
+    int lunghezza = calcola_lunghezza(vettore);
+    calcola_media(lunghezza, vettore);
 
     return 0;
 }
